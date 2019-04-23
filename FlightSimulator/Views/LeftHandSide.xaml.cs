76ef33a79cd9707313a5b60ffb6e9bfcda765fc9
Amit Ashkenazi <amit471@gmail.com>
2019-04-23 20:33:37 +0300

    Added all of the "view" files, but they are still incomplete.

---
+++ FlightSimulator/Views/LeftHandSide.xaml.cs
@@ -0,0 +1,29 @@
+﻿using FlightSimulator.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FlightSimulator.Views
+{
+    /// <summary>
+    /// Interaction logic for LeftHandSide.xaml
+    /// </summary>
+    public partial class LeftHandSide : UserControl
+    {
+        public LeftHandSide()
+        {
+            InitializeComponent();
+            DataContext = new ConnectAndSettingsVM();
+        }
+    }
+}
---
+++ FlightSimulator/Views/RightHandSide.xaml.cs
@@ -0,0 +1,28 @@
+﻿using FlightSimulator.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FlightSimulator.Views
+{
+    /// <summary>
+    /// Interaction logic for RightHandSide.xaml
+    /// </summary>
+    public partial class RightHandSide : UserControl
+    {
+        public RightHandSide()
+        {
+            InitializeComponent();
+        }
+    }
+}
