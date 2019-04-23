4caee7a4df6a87e2bafb8b9dc11112e2bf0b1d23
אורי לייבוביץ <ori.leibo@gmail.com>
2019-04-23 18:03:57 +0300

    Add project files.

---
+++ FlightSimulator/Model/Connection/Commands.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightSimulator.Model.Connection
+{
+    class Commands
+    {
+        private TcpClient client;
+        private BinaryWriter writer; // writer
+        //private NetworkStream streamC;
+        public bool isConnected = false;
+        #region Singleton
+        private static Commands _Instance = null;
+        public static Commands Instance
+        {
+            get
+            {
+                if (_Instance == null)
+                {
+                    _Instance = new Commands();
+                }
+                return _Instance;
+            }
+        }
+        #endregion
+        public void ComConnect(string ip, int port)
+        {
+            client = new TcpClient();
+            IPEndPoint ipEndPo = new IPEndPoint(IPAddress.Parse(ip), port);
+            while (!client.Connected)
+            {
+                try { client.Connect(ipEndPo); }
+                catch (Exception exp)
+                {
+                    Console.WriteLine("pay attention to: {0}", exp.ToString());
+                }
+            }
+            writer = new BinaryWriter(client.GetStream());
+            Console.WriteLine("Connected");
+            isConnected = true;
+        }
+        public void ComSend(string message)
+        {
+            if (string.IsNullOrEmpty(message)) return;
+            string[] commendsList = message.Split('\n');
+            foreach (string s in commendsList)
+            {
+                string indiCom = s + "\r\n";
+                byte[] sendMess = ASCIIEncoding.ASCII.GetBytes(indiCom);
+                writer.Write(sendMess, 0, sendMess.Length);
+                System.Threading.Thread.Sleep(2000);
+            }
+
+
+        }
+        public void ComClear()
+        {
+            _Instance = null;
+        }
+        public void ComClose()
+        {
+            client.Close();
+            writer.Close();
+        }
+
+    }
+}
+
---
+++ FlightSimulator/Model/Connection/Info.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightSimulator.Model.Connection
+{
+    class Info
+    {
+
+        private TcpClient client;
+        private BinaryReader reader;
+        private TcpListener listener;
+        public bool isStop = false;
+        public bool isConnected = false;
+
+        public string[] Read()
+        {
+            if (isConnected == false)
+            {
+                client = listener.AcceptTcpClient();
+                reader = new BinaryReader(client.GetStream());
+                isConnected = true;
+            }
+
+            char c;
+            string s = "";
+            while ((c = reader.ReadChar()) != '\n')
+            {
+                s += c;
+            }
+            string[] val = s.Split(',');
+            string[] retVal = { val[0], val[1] };
+            return retVal;
+        }
+        public void Connect(string ip, int port)
+        {
+            if (listener != null)
+            {
+                Disconnect();
+            }
+            listener = new TcpListener(IPAddress.Any, port);
+            listener.Start();
+            Console.WriteLine("connected");
+        }
+
+        public void Disconnect()
+        {
+            isConnected = false;
+            client.Close();
+        }
+        public void Listener_Stop()
+        {
+            isStop = true;
+            listener.Stop();
+        }
+
+    }
+}
